e48551797d2f575b6eacc84348a912bbc7ec5004
Luis Pool <Luis_Pool@epam.com>
2017-10-19 12:35:04 -0500

    * Remove App.Mvc and App.Api projects. * Add App.Web.Mvc project with scaffolded content.

---
+++ App.IntegrationTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace App.IntegrationTests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public HomeControllerTests()
+        {
+            // Arrange
+            var webHostBuilder = new WebHostBuilder()
+                .UseContentRoot(@"D:\LibDev\GIT\DotNetCore\ASP\IntegrationTestsWithTestServer\App.Web.Mvc")
+                .UseStartup<App.Web.Mvc.Startup>();
+
+            var _testServer = new TestServer(webHostBuilder);
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [Fact]
+        public async Task GivenAboutAction_WhenTextIsDisplayed_ThenViewDataTextShouldBeExpected()
+        {
+            // Act
+            var response = await _httpClient.GetAsync("/Home/About");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var responseHtml = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Your application description page.", responseHtml);
+        }
+    }
+}
